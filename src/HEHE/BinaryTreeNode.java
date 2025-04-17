20ed1cbd68150438c80d66e33d00acd7b5b57a5b
TheGOHF <119022328+TheGOHF@users.noreply.github.com>
2025-04-16 18:00:28 -1000

    Create BinaryTreeNode.java

---
+++ src/HEHE/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+class BinaryTreeNode {
+    int value;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    public BinaryTreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
